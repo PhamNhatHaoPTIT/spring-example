61a76c8b63a4ecf28cb6733111bce5890b59a828
PhamNhatHaoPTIT <nhathaokp8@gmail.com>
2019-11-15 14:19:49 +0700

    fix save error

--- redisson-demo/src/main/java/com/haopn/demo/service/CounterServiceImpl.java
+++ redisson-demo/src/main/java/com/haopn/demo/service/CounterServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
@@ -25,66 +26,42 @@ public class CounterServiceImpl implements CounterService {
     public void init() {
         client = Redisson.create();
         atomicLong = client.getAtomicLong("counter");
+        Count count = new Count();
+        count.setNumber(0);
+        countRepository.save(count);
     }
 
     @Override
     public void resetCounter() {
-        RLock lock = client.getLock("lock");
-        lock.lock();
-        try {
-            atomicLong.set(0);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            lock.unlock();
-        }
+        atomicLong.set(0);
     }
 
     @Override
     public void increaseCounter() {
-        RLock lock = client.getLock("lock");
-        lock.lock();
-        try {
-            atomicLong.incrementAndGet();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            lock.unlock();
-        }
+        atomicLong.incrementAndGet();
     }
 
     @Override
     public int getCounterRedis() {
-        int counter = 0;
-        RLock lock = client.getLock("lock");
-        lock.lock();
-        try {
-            counter = (int) atomicLong.get();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            lock.unlock();
-        }
-        return counter;
+        return (int) atomicLong.get();
     }
 
     @Override
     public void persisCounter() {
         Count count = countRepository.findById(1);
-        count.setNumber(getCounterRedis() + count.getNumber());
+        int currentCount = getCounterRedis();
+        count.setNumber(currentCount + count.getNumber());
     }
 
     @Override
     @Scheduled(fixedDelay = 1000)
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRED)
     public void setSchedule() {
         RLock lock = client.getLock("lock");
         lock.lock();
         try {
             persisCounter();
             resetCounter();
-        } catch (Exception e) {
-            e.printStackTrace();
         } finally {
             lock.unlock();
         }
